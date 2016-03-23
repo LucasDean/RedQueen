8a691b15e2c0658751d7dbc79801b47519fe3551
LucasDean <deanlm90@gmail.com>
2016-03-22 22:29:56 -0600

    Update RedQueen_Uno.ino

--- RedQueen_Uno.ino
+++ RedQueen_Uno.ino
@@ -5,15 +5,15 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, PIN, NEO_RGB + NEO_KHZ800);
 #include <Wire.h>
 #define SLAVE_ADDRESS 0X04
 
-unsigned long previousMillis = 0;        // will store last time LED was updated
-const long interval = 1000;           // interval at which to blink (milliseconds)
-int data = 0;  // Store integer commands sent from Pi
-int pinval[6];  // Array to store 6 analogRead values
-int previous_pinval[] = {0, 0, 0, 0, 0, 0}; //Store previous pin val
-int zcode[6];  // Array to store zone codes
-int zone = 0;  // Zone iterative counter
+unsigned long previousMillis = 0;
+const long interval = 1000;
+int data = 0;
+int pinval[6];
+int previous_pinval[] = {0, 0, 0, 0, 0, 0};
+int zcode[6];
+int zone = 0;
 int active_zones = 1; // Total zones active using low pin #s first
-int code = 8;  // Dummy value for code until codeLookup complete
+int code = 8;
 int previous_code = 0;
 int entryway[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 int entryways = active_zones * 4;
@@ -25,36 +25,49 @@ String mode = "Passive";
 int brightness = 20;
 
 void setup() {
-  strip.begin(); // LEDs
-  strip.show(); // Initialize all pixels to 'off'
+  strip.begin();
+  strip.show();
   for (int j=0; j<strip.numPixels(); j++){
-    strip.setPixelColor(j, strip.Color(0,brightness,0));    // Set fourth LED to full red, no green, full blue
+    strip.setPixelColor(j, strip.Color(0,brightness,0));
     strip.show(); 
   }
-//  Serial.begin(9600);  // Open Serial with PC
+  Serial.begin(9600);
   Wire.begin(SLAVE_ADDRESS);
-  Wire.onRequest(requestEvent); // register event
+  Wire.onRequest(requestEvent);
   Wire.onReceive(receiveEvent);
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
 }
 
 void loop() {
-  for(zone=0;zone<active_zones;zone++){                                           // For zone 0
+  if (digitalRead(11) == HIGH && digitalRead(12) == LOW) {
+    mode = "Active";
+  }
+  else if (digitalRead(11) == LOW && digitalRead(12) == HIGH) {
+    mode = "Passive";
+  }
+  else {
+      for (int i = 0; i < entryways; i++) {
+        strip.setPixelColor(i, strip.Color(0,0,150));
+        strip.show();
+      }
+  }
+  for(zone=0;zone<active_zones;zone++){ //-------------------------------------------------------------------------------------------------------------                                    
     do {
-      int reading_1 = analogRead(zone);
-      int reading_2 = analogRead(zone);
-      int reading_3 = analogRead(zone);
-      int reading_4 = analogRead(zone);
-      int reading_5 = analogRead(zone);
-      reading_average = (reading_1+reading_2+reading_3+reading_4+reading_5)/5;    // average five readings of pin 0
-//      Serial.println(reading_average);
-      pinval[zone]=reading_average;                                               // pinval[0] = average value
-      delta = pinval[zone]-previous_pinval[zone];                                 // difference between old pinval[0] and new pinval[0]
-      val_delta = abs(delta);                                                     // absolute value of this difference
-      previous_pinval[zone] = pinval[zone];                                       // define new old pinval[0] value
-    } while (val_delta > 4 );                                                     // keep looping until two consecutive averages are less than or equal to 4
+      int sum = 0;
+      for (int i=0; i<10; i++) {
+        sum = sum + analogRead(zone);
+      }
+      reading_average = sum / 10;                                            // average 10 readings of pin 0
+      Serial.println(reading_average);
+      pinval[zone]=reading_average;                                          // pinval[0] = average value
+      delta = pinval[zone]-previous_pinval[zone];                            // difference between old pinval[0] and new pinval[0]
+      val_delta = abs(delta);                                                // absolute value of this difference
+      previous_pinval[zone] = pinval[zone];                                  // define new old pinval[0] value
+    } while (val_delta > 4 );                                                // keep looping until two consecutive averages are less than or equal to 4
     int index = zone * 4 - 1;
     if ( pinval[zone] < 1020 && pinval[zone] >= 898 ){                       // READING: 898 - 1020
-      if (mode == "Passive") {                                                // CODE: 26  
+      if (mode == "Passive") {                                               // CODE: 26  
         entryway[index+1] = 0;                                               // OPEN: NONE
         entryway[index+2] = 0;                                               
         entryway[index+3] = 0;
@@ -63,7 +76,7 @@ void loop() {
     
     else if ( pinval[zone] < 898 && pinval[zone] >= 699 ) {                  // READING: 699 - 898
       entryway[index+1] = 1;                                                 // CODE: 25
-      if (mode == "Passive") {                                                // OPEN: 1
+      if (mode == "Passive") {                                               // OPEN: 1
         entryway[index+2] = 0;
         entryway[index+3] = 0;
         entryway[index+4] = 0; }
@@ -71,7 +84,7 @@ void loop() {
 
     else if ( pinval[zone] < 699 && pinval[zone] >= 562 ) {                  // READING: 562 - 699                        
       entryway[index+2] = 1;                                                 // CODE: 24
-      if (mode == "Passive") {                                                // OPEN: 2
+      if (mode == "Passive") {                                               // OPEN: 2
         entryway[index+1] = 0;
         entryway[index+3] = 0;
         entryway[index+4] = 0; }
@@ -79,7 +92,7 @@ void loop() {
     
     else if ( pinval[zone] < 457 && pinval[zone] >= 377 ) {                  // READING: 377 - 457
       entryway[index+3] = 1;                                                 // CODE: 22
-      if (mode == "Passive") {                                                // OPEN: 3
+      if (mode == "Passive") {                                               // OPEN: 3
         entryway[index+1] = 0;
         entryway[index+2] = 0;
         entryway[index+4] = 0; }                                             
@@ -87,7 +100,7 @@ void loop() {
     
     else if ( pinval[zone] < 280 && pinval[zone] >= 261 ) {                  // READING: 261 - 280
       entryway[index+4] = 1;                                                 // CODE: 18
-      if (mode == "Passive") {                                                // OPEN: 4
+      if (mode == "Passive") {                                               // OPEN: 4
         entryway[index+1] = 0;                                              
         entryway[index+2] = 0;
         entryway[index+3] = 0; }  
